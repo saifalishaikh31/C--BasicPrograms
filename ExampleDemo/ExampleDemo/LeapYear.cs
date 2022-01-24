7871b2cdaa7be36e255fda8df3dc7a51b642a26b
saifalishaikh31 <saifalishaikh31>
2022-01-24 08:20:22 +0530

    [Saif Ali] Add: New Program Added

---
+++ ExampleDemo/ExampleDemo/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExampleDemo
+{
+    class LeapYear
+    {
+        public void CheckLeapYear()
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year.", Year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year.", Year);
+            }
+            Console.ReadLine();  
+        }
+    }
+}
--- ExampleDemo/ExampleDemo/Program.cs
+++ ExampleDemo/ExampleDemo/Program.cs
@@ -9,7 +9,30 @@ namespace ExampleDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+             bool flag = true;
+             while (flag)
+             {
+                 Console.WriteLine("1.FlipCoin. \n 2.LeapYear. \n 3.Exit");
+                 int option = Convert.ToInt32(Console.ReadLine());
+                 switch (option)
+                 {
+                     case 1:
+                         FlipCoin flipcoin = new FlipCoin();
+                         flipcoin.HeadTailPercentage();
+                         break;
+                     case 2:
+                         LeapYear leapyear = new LeapYear();
+                         leapyear.CheckLeapYear();
+                         break;
+                     case 3: flag = false;
+                         break;
+                     default: Console.WriteLine("Choose Correct option");
+                         break;
+                 }
+
+
+
+             }   
         }
     }
 }
